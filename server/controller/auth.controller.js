91f50c971e68dcf837bb2e6621a6f4beb51275ec
Kanha-1234 <161133758+Kanha-1234@users.noreply.github.com>
2024-12-11 17:29:25 +0530

    mail vefification

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,134 +1,149 @@
-import jwt from "jsonwebtoken"
-import bcrypt from "bcryptjs"
-import userModel from "../models/user.model.js"
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import userModel from "../models/user.model.js";
+import transporter from "../config/nodemailer.js";
 
-export const register = async(req,res)=>{
-    const {name,email,password} = req.body
-    if(!name || !email || !password){
-        return res.status(400).json({
-            success:false,
-            message:"all fields are required"
-        })
-    }
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "all fields are required",
+    });
+  }
 
-    try {
+  try {
+    const existingUser = await userModel.findOne({ email });
 
-        const existingUser = await userModel.findOne({email})
-
-        if(existingUser){
-            return res.status(400).json({
-                success:false,
-                message:"user already exit"
-            })
-        }
-
-
-      const hashedPassword = await bcrypt.hash(password,10)
-
-      const newuser = new userModel({
-        name,email,password:hashedPassword
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "user already exit",
       });
+    }
 
-      await newuser.save();
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-      const registerToken = jwt.sign({id:newuser._id},process.env.JWT_SEKRET,{expiresIn:'7d'})
-        
+    const newuser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
-       res.status(201).cookie("token",registerToken,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV === "production",
-        sameSite:process.env.NODE_ENV == "production" ? "none":"strict",
-        maxAge:7*24*60*60*1000
-      })
+    await newuser.save();
 
+    const registerToken = jwt.sign(
+      { id: newuser._id },
+      process.env.JWT_SEKRET,
+      { expiresIn: "7d" }
+    );
+
+    res.status(201).cookie("token", registerToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV == "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    //mail service
+
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: `registration successfull`,
+      text: `hey ${name} welcome to our web site`,
+    };
+
+    await transporter.sendMail(mailOptions);
+    return res.json({
+      success: true,
+    });
+  } catch (error) {
+    res.json({
+      succes: false,
+      message: error.message,
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.json({
+      success: false,
+      message: "both fields are required",
+    });
+  }
+
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
       return res.json({
-        success:true
-      })
-        
-    } catch (error) {
-        res.json({
-            succes:false,
-            message:error.message
-        })
-    }
-}
-
-
-export const login = async(req,res)=>{
-    const {email,password} = req.body;
-
-    if(!email || !password){
-        return res.json({
-            success:false,
-            message:"both fields are required"
-
-        })
+        success: false,
+        message: "user does not found with this email",
+      });
     }
 
-    try {
-
-        const user = await userModel.findOne({email})
-        if(!user){
-            return res.json({
-                success:false,
-                message:"user does not found with this email"
-            })
-        }
-
-        const ismatch = await bcrypt.compare(password,user.password);
-        if(!ismatch){
-            return res.json({
-                success:false,
-                message:"invalid email or password"
-            })
-        }
-
-        const loginToken = jwt.sign({id:user._id},process.env.JWT_SEKRET,{expiresIn:'7d'})
-        
-
-       res.status(201).cookie("token",loginToken,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV === "production",
-        sameSite:process.env.NODE_ENV == "production" ? "none":"strict",
-        maxAge:7*24*60*60*1000
-      })
-
+    const ismatch = await bcrypt.compare(password, user.password);
+    if (!ismatch) {
       return res.json({
-        success:true
-      })
-        
-    } catch (error) {
-        return res.json({
-            success:false,
-            message:error.message
-        })
+        success: false,
+        message: "invalid email or password",
+      });
     }
-}
 
-export const logout = async(req,res)=>{
-    try {
-        
-        res.status(200).cookie("token",{
-            httpOnly:true,
-            secure:process.env.NODE_ENV === "production",
-            sameSite:process.env.NODE_ENV == "production" ? "none":"strict",
-            maxAge:7*24*60*60*1000
-          })
-    
-          return res.json({
-            success:true,
-            message:"logged out"
-          })
+    const loginToken = jwt.sign({ id: user._id }, process.env.JWT_SEKRET, {
+      expiresIn: "7d",
+    });
 
+    res.status(201).cookie("token", loginToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV == "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
 
+    return res.json({
+      success: true,
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
+export const logout = async (req, res) => {
+  try {
+    res.status(200).cookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV == "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
 
+    return res.json({
+      success: true,
+      message: "logged out",
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
+//send verification account
+export const sendVerifyOtp = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const user = await userModel.findById({ userId });
 
-    } catch (error) {
-        return res.json({
-            success:false,
-            message:error.message
-        })
+    if (user.isAccoutVerified) {
+      return res.json({ success: false, message: "account already verified" });
     }
-}
+  } catch (error) {}
+};
