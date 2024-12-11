b8adcceaff18a065b3a5038a0b3fb9e01120b799
Kanha-1234 <161133758+Kanha-1234@users.noreply.github.com>
2024-12-11 15:57:16 +0530

    login controller success

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -31,19 +31,75 @@ export const register = async(req,res)=>{
 
       await newuser.save();
 
-      const token = jwt.sign({id:newuser._id},process.env.JWT_SEKRET,{expiresIn:'7d'})
+      const registerToken = jwt.sign({id:newuser._id},process.env.JWT_SEKRET,{expiresIn:'7d'})
         
 
-       res.staus(201).cookie("token",token,{
+       res.staus(201).cookie("token",registerToken,{
         httpOnly:true,
         secure:process.env.NODE_ENV === "production",
         sameSite:process.env.NODE_ENV == "production" ? "none":"strict",
         maxAge:7*24*60*60*1000
       })
+
+      return res.json({
+        success:true
+      })
         
     } catch (error) {
         res.json({
             succes:false,
+            message:error.message
+        })
+    }
+}
+
+
+export const login = async(req,res)=>{
+    const {email,password} = req.body;
+
+    if(!email || !password){
+        return res.json({
+            success:false,
+            message:"both fields are required"
+
+        })
+    }
+
+    try {
+
+        const user = await undefined.findOne({email})
+        if(!user){
+            return res.json({
+                success:false,
+                message:"user does not found with this email"
+            })
+        }
+
+        const ismatch = await bcrypt.compare(password,user.password);
+        if(!ismatch){
+            return res.json({
+                success:false,
+                message:"invalid email or password"
+            })
+        }
+
+        const loginToken = jwt.sign({id:newuser._id},process.env.JWT_SEKRET,{expiresIn:'7d'})
+        
+
+       res.staus(201).cookie("token",loginToken,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV === "production",
+        sameSite:process.env.NODE_ENV == "production" ? "none":"strict",
+        maxAge:7*24*60*60*1000
+      })
+
+      return res.json({
+        success:true
+      })
+        
+    } catch (error) {
+        return res.json({
+            success:false,
             message:error.message
         })
     }
